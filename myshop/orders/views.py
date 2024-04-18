b4c356c8d4cbb132fe694f643bd4639ac77b73be
yorgopetsas <yorgopecas@gmail.com>
2024-04-18 17:58:57 +0200

    Stripe Payment implemented and tested

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
+from django.urls import reverse
+
 
 def order_create(request):
     cart = Cart(request)
@@ -15,8 +17,14 @@ def order_create(request):
                 OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
 
             cart.clear()
+            # launch asynchronous task
             order_created.delay(order.id)
-            return render(request, 'orders/order/created.html', {'order': order})
+            # set the order in the session
+            request.session['order_id'] = order.id
+            # redirect for payment
+            return redirect(reverse('payment:process'))
+
+            # return render(request, 'orders/order/created.html', locals())
         
     else:
         form = OrderCreateForm()    
