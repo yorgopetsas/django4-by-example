27f3043e605598e3bfd8230bbb85a7f54ebde14a
yorgopetsas <yorgopecas@gmail.com>
2024-04-19 12:29:23 +0200

    Small touches

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Category, Product
 from orders.models import Order, OrderItem
+# from django.utils.safestring import mark_safe
+
+import csv
+import datetime
+from django.http import HttpResponse
+
+# from coupons.models import Coupon
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -18,8 +25,37 @@ class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
 
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f'attachment; filename={opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
+    for obj in queryset:
+        data_row = []
+    for field in fields:
+        value = getattr(obj, field.name)
+        if isinstance(value, datetime.datetime):
+            value = value.strftime('%d/%m/%Y')
+        data_row.append(value)
+    writer.writerow(data_row)
+    return response
+
+export_to_csv.short_description = 'Export to CSV'
+
 @admin.register(Order)    
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
+
+# @admin.register(Coupon)
+# class CouponAdmin(admin.ModelAdmin):
+#     list_display = ['code', 'valid_from', 'valid_to', 'discount', 'active']
+#     list_filter = ['active', 'valid_from', 'valid_to']
+#     search_fields = ['code']
