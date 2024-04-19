6650f08e42acb2c37cac168769e4aec50f9bdc33
yorgopetsas <yorgopecas@gmail.com>
2024-04-19 12:27:14 +0200

    Adapt cart for coupons

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from shop.models import Product
+from coupons.models import Coupon
 
 class Cart:
     def __init__(self, request):
@@ -9,6 +10,24 @@ class Cart:
         if not cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+        # store current applied coupon
+        self.coupon_id = self.session.get('coupon_id')
+
+    # Iterate over the items in the cart and get the products from the database.
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
 
     def add(self, product, quantity=1, override_quantity=False):
         product_id = str(product.id)
@@ -28,27 +47,31 @@ class Cart:
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
-    # Iterate over the items in the cart and get the products from the database.
-    def __iter__(self):
-        product_ids = self.cart.keys()
-        products = Product.objects.filter(id__in=product_ids)
-        cart = self.cart.copy()
-        for product in products:
-            cart[str(product.id)]['product'] = product
-        for item in cart.values():
-            item['price'] = Decimal(item['price'])
-            item['total_price'] = item['price'] * item['quantity']
-            yield item
-    
-    def __len__(self):
-        return sum(item['quantity'] for item in self.cart.values())
-    
-    def get_total_price(self):
-        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
-    
+
     def clear(self):
         del self.session[settings.CART_SESSION_ID]
         self.save()
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+    
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+        return None
+    
+    def get_discount(self):
+        if self.coupon:
+            return Decimal((self.coupon.discount * self.get_total_price()) / 100)
+        return Decimal(0)
+    
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
+    
 
 
 
